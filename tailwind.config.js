e432ab0885ba6e27456315f58566f478b8d7343d
Andrew Priddle-Higson <andrewgph@gmail.com>
2023-12-01 10:57:35 -0800

    adding tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // path to all JS and JSX files
+    // './public/index.html' // Include this if you have HTML files in public directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
