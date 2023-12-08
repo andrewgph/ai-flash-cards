5401ac69cc8e8aa76135d36fcb1868d519033c5f
Andrew Priddle-Higson <andrewgph@gmail.com>
2023-12-07 23:57:28 -0800

    allow loading JSON format decks

--- src/components/DeckLoader.js
+++ src/components/DeckLoader.js
@@ -14,27 +14,63 @@ const DeckLoader = ({ onLoad }) => {
     setIsFileLoaderVisible(false);
   };
 
+  // TODO: Could do more validation here.
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        const deck = {
-          title: file.name, 
-          cards: results.data.map((row, index) => ({
-            id: index, 
-            front: row.front, 
-            back: row.back,
-            repetitions: row.repetitions ? row.repetitions : 0,
-            interval: row.interval ? row.interval : 0,
-            easeFactor: row.easeFactor ? row.easeFactor : 2.5,
-            lastReviewedDate: row.lastReviewedDate ? row.lastReviewedDate : null
-          })),
-        };
-        console.log('Parsed deck:', deck);
-        handleFileLoaded(deck);
-      }
-    });
+    const extension = file.name.split('.').pop().toLowerCase();
+
+    if (extension === 'csv') {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          const deck = {
+            title: file.name, 
+            cards: results.data.map((row, index) => ({
+              id: index, 
+              front: row.front, 
+              back: row.back,
+              repetitions: row.repetitions ? row.repetitions : 0,
+              interval: row.interval ? row.interval : 0,
+              easeFactor: row.easeFactor ? row.easeFactor : 2.5,
+              lastReviewedDate: row.lastReviewedDate ? row.lastReviewedDate : null
+            })),
+          };
+          console.log('Parsed deck:', deck);
+          handleFileLoaded(deck);
+        }
+      });
+    } else if (extension === 'json') {
+      const reader = new FileReader();
+
+      reader.onload = function(event) {
+        try {
+          let jsonObj = JSON.parse(event.target.result);
+          console.log('Parsed JSON:', jsonObj);
+
+          // Validate and initialize missing fields
+          if (!jsonObj.title) jsonObj.title = file.name;
+          if (!jsonObj.cards) jsonObj.cards = [];
+
+          jsonObj.cards = jsonObj.cards.map((card, index) => ({
+            id: card.id !== undefined ? card.id : index,
+            front: card.front || '',
+            back: card.back || '',
+            repetitions: card.repetitions || 0,
+            interval: card.interval || 0,
+            easeFactor: card.easeFactor || 2.5,
+            lastReviewedDate: card.lastReviewedDate || null
+          }));
+
+          handleFileLoaded(jsonObj);
+        } catch (err) {
+          console.error('Could not parse JSON file:', err);
+        }
+      };
+
+      reader.readAsText(file);
+    } else {
+      console.error('Unsupported file type:', extension);
+    }
   };
 
   return (
